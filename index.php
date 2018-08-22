01bc1ee83d40b50eece70ccc7eaf32976470c192
Cristian Andres Afanador Delgado <caafanadord@ufpso.edu.co>
2018-08-22 10:50:28 -0500

    index

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
+use Divisis\Kernel\Router\Router;
+
 #definición de path
 define('_ROOT_', '');
 
 #importar configuración
 require _ROOT_ . 'framework/kernel/config/config.php';
+# Ejecutar controlador solicitado
+
+(new Router)->executeController();
