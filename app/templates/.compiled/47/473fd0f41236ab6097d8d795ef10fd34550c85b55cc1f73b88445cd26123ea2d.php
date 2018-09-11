90561cd97d9b56554087092fe0b237af0e31595e
Cristian Andres Afanador Delgado <caafanadord@ufpso.edu.co>
2018-09-11 16:43:57 -0500

    login

--- app/templates/.compiled/47/473fd0f41236ab6097d8d795ef10fd34550c85b55cc1f73b88445cd26123ea2d.php
+++ app/templates/.compiled/47/473fd0f41236ab6097d8d795ef10fd34550c85b55cc1f73b88445cd26123ea2d.php
@@ -29,22 +29,24 @@ class __TwigTemplate_54870346773416ad9476655b3fc15b176c949bfbe64edcb4934cebb901e
     <body>
         <div class='row'>
             <div class='cuerpo card col l4 m6 s12'>
-                <div class=\"input-field  s6\">
-                    <input placeholder=\"Usuario\" id=\"usuario\" type=\"text\" class=\"validate\">
-                    <label for=\"usuario\">Usuario</label>
+                <h5 class=\"card-title\">Inicio de sesión</h5>
+                <div class='card-content'>
+                    <div class=\"input-field  s6\">
+                        <input placeholder=\"Usuario\" id=\"usuario\" type=\"text\" class=\"validate\">
+                        <label for=\"usuario\">Usuario</label>
+                    </div>
+                    <div class=\"input-field s6\">
+                        <input placeholder=\"Contraseña\" id=\"password\" type=\"password\" class=\"validate\">
+                        <label for=\"password\">Contraseña</label>
+                    </div>
                 </div>
-                <div class=\"input-field s6\">
-                    <input placeholder=\"Contraseña\" id=\"password\" type=\"password\" class=\"validate\">
-                    <label for=\"password\">Contraseña</label>
-                </div>
-                <div>
-                    <button class='btn btn-large '>Ingresar</button>
+                <div class='card-action'>
+                    <a href=\"#\">Ingresar</a>
                 </div>
             </div>
-        </div>
-        <footer>
-            <script src=\"assets/vendors/materialize/js/materialize.min.js\"></script>
-        </footer>
+            <footer>
+                <script src=\"assets/vendors/materialize/js/materialize.min.js\"></script>
+            </footer>
     </body>
 </html>";
     }
@@ -71,22 +73,24 @@ class __TwigTemplate_54870346773416ad9476655b3fc15b176c949bfbe64edcb4934cebb901e
     <body>
         <div class='row'>
             <div class='cuerpo card col l4 m6 s12'>
-                <div class=\"input-field  s6\">
-                    <input placeholder=\"Usuario\" id=\"usuario\" type=\"text\" class=\"validate\">
-                    <label for=\"usuario\">Usuario</label>
+                <h5 class=\"card-title\">Inicio de sesión</h5>
+                <div class='card-content'>
+                    <div class=\"input-field  s6\">
+                        <input placeholder=\"Usuario\" id=\"usuario\" type=\"text\" class=\"validate\">
+                        <label for=\"usuario\">Usuario</label>
+                    </div>
+                    <div class=\"input-field s6\">
+                        <input placeholder=\"Contraseña\" id=\"password\" type=\"password\" class=\"validate\">
+                        <label for=\"password\">Contraseña</label>
+                    </div>
                 </div>
-                <div class=\"input-field s6\">
-                    <input placeholder=\"Contraseña\" id=\"password\" type=\"password\" class=\"validate\">
-                    <label for=\"password\">Contraseña</label>
-                </div>
-                <div>
-                    <button class='btn btn-large '>Ingresar</button>
+                <div class='card-action'>
+                    <a href=\"#\">Ingresar</a>
                 </div>
             </div>
-        </div>
-        <footer>
-            <script src=\"assets/vendors/materialize/js/materialize.min.js\"></script>
-        </footer>
+            <footer>
+                <script src=\"assets/vendors/materialize/js/materialize.min.js\"></script>
+            </footer>
     </body>
 </html>", "login.twig", "/var/www/html/interno/app/views/login.twig");
     }
